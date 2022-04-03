d11373af5f813a81c1b35eecd4196bf47e262031
Ninad <ninadnaik07@gmail.com>
2022-04-03 12:56:21 +0530

    Add comments

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+
+//Function declaration for GCV
 float gcv(float, float, float, float);
-float ncv(float,float, float);
+
+//Function declaration for NCV
+float ncv(float, float, float);
+
 void main(){
     printf("This is a calculator for Gross Calorific Value (GCV) and Net Calorific Value (NCV)\nEnter the percentage value of the elements in the given fuel\n");
     float carbon, hydrogen, oxygen, sulphur, latentHeat = 587, GCV;
@@ -14,13 +19,17 @@ void main(){
     scanf("%f", &sulphur);
     printf("Latent Heat of condensation of steam (default: 587 cal/g): ");
     scanf("%f", &latentHeat);
-    GCV = gcv(carbon, hydrogen, oxygen, sulphur);
+    GCV = gcv(carbon, hydrogen, oxygen, sulphur);  //function call
     printf("Gross Calorific Value (GCV) = %.2f kcal/kg\n", GCV);
     printf("Net Calorific Value (NCV) = %.2f kcal/kg\n", ncv(GCV, hydrogen, latentHeat));
 }
+
+//Function definition for GCV
 float gcv(float carbon, float hydrogen, float oxygen, float sulphur){
     return (8080*carbon+34500*(hydrogen-oxygen/8)+2240*sulphur)/100;
 }
+
+//Function definition for NCV
 float ncv(float GCV, float hydrogen, float latentHeat){
     return GCV-9*hydrogen*latentHeat/100;
 }
